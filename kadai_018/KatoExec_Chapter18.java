59247599fa1d158bf440af97cd81754139470697
KS0811903 <fw7lk3rts@gmail.com>
2025-07-22 23:11:26 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,23 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		//各子クラスのオブジェクトを作る
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		
+		//set.GivenNameメソッドを実行
+		taro.setGivenName("太郎");
+		ichiro.setGivenName("一郎");
+		hanako.setGivenName("花子");
+		
+		//紹介メソッドを実行
+		taro.execIntroduce();
+		ichiro.execIntroduce();
+		hanako.execIntroduce();
+
+	}
+
+}
