7fecd63e483049cf2ae82745ebf7718a16f19fec
KS0811903 <fw7lk3rts@gmail.com>
2025-07-22 17:52:01 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,18 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//ギアデータ生成
+		Car_Chapter15 gear = new Car_Chapter15();
+		
+		//ギア3に変更する
+		gear.changeGear(3);
+		gear.run();
+		
+		
+
+	}
+
+}
